452577900c8c89b1ec70fb5f7f338020bbbb0189
genizy <185154684+genizy@users.noreply.github.com>
2026-01-31 15:51:44 -0800

    Change const to let for files array and remove button

--- games.js
+++ games.js
@@ -1,4 +1,4 @@
-const files = [
+let files = [
   'cl1',
 'cl10bullets',
 'cl10minutestildawn',
@@ -2201,7 +2201,7 @@ const files = [
 'clvvvvvv',
 'clwilywars',
 ];
-
+document.getElementById("lolbutton").remove();
         function generateAllSections() {
             const allChars = ['0','1','2','3','4','5','6','7','8','9',
                             'A','B','C','D','E','F','G','H','I','J','K','L','M',
