97af5290a856504ec4dfaec445290d1e70c38bf7
genizy <185154684+genizy@users.noreply.github.com>
2026-01-31 16:02:35 -0800

    fgdfh

--- games.js
+++ games.js
@@ -2201,9 +2201,10 @@ let files = [
     'clvvvvvv',
     'clwilywars',
 ];
-document.getElementById("lolbutton").remove();
-
 function generateAllSections() {
+    try {
+document.getElementById("lolbutton").remove();
+} catch(e){}
     const allChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
